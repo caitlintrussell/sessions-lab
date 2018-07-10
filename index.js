2742a83e8ff8c74ddb3e72efd6574cca66411cf5
Caitlin Trussell <caitlintrussell@gmail.com>
2018-07-10 10:49:16 -0500

    basic session created

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ app.use(session({
   saveUninitialized: true,
 }));
 
+app.use((req, res, next) => {
+  console.log('SESSION: ', req.session);
+  next();
+});
+
+app.use((req, res, next) => {
+  if (!req.session.counter) req.session.count = 0;
+  console.log('counter', ++req.session.count);
+  next();
+});
+
 app.get('/', (req, res, next) => {
   res.send('SUP?');
 });
